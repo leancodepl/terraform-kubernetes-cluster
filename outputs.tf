f7455993ea7421db808c01ece6a47312893db1a8
Jakub Fijałkowski <jakub.fijalkowski@leancode.pl>
2020-03-23 12:54:24 +0100

    Export DNS Zone resource group

--- outputs.tf
+++ outputs.tf
@@ -41,9 +41,10 @@ output "cluster_id" {
 
 output "domain" {
   value = {
-    zone_id      = azurerm_dns_zone.cluster_domain.id
-    zone_name    = azurerm_dns_zone.cluster_domain.name
-    name_servers = azurerm_dns_zone.cluster_domain.name_servers
+    zone_id             = azurerm_dns_zone.cluster_domain.id
+    zone_name           = azurerm_dns_zone.cluster_domain.name
+    resource_group_name = azurerm_dns_zone.cluster_domain.resource_group_name
+    name_servers        = azurerm_dns_zone.cluster_domain.name_servers
   }
 }
 
