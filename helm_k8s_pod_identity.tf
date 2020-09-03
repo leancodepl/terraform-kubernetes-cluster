ec01fb9c410765b4aab01ca48b1353bf6cda38a6
Jakub Fijałkowski <jakub.fijalkowski@leancode.pl>
2020-09-03 13:25:13 +0200

    Put the `aad-pod-identity` Helm into a separate namespace

--- helm_k8s_pod_identity.tf
+++ helm_k8s_pod_identity.tf
@@ -1,3 +1,9 @@
+resource "kubernetes_namespace" "aad_pod_identity" {
+  metadata {
+    name = "aad-pod-identity"
+  }
+}
+
 resource "helm_release" "aad_pod_identity" {
   count = var.deploy_aad_pod_identity ? 1 : 0
 
@@ -7,18 +13,23 @@ resource "helm_release" "aad_pod_identity" {
   version    = "2.0.1"
 
   set {
-    name  = "forceNameSpaced"
-    value = true
-  }
-
-  set {
     name  = "mic.tag"
     value = "1.6.2"
   }
 
   set {
+    name  = "mic.leaderElection.namespace"
+    value = kubernetes.kubernetes_namespace.aad_pod_identity.metadata[0].name
+  }
+
+  set {
     name  = "nmi.tag"
     value = "1.6.2"
+  }
+
+  set {
+    name  = "nmi.micNamespace"
+    value = kubernetes.kubernetes_namespace.aad_pod_identity.metadata[0].name
   }
 
   set {
@@ -31,4 +42,8 @@ resource "helm_release" "aad_pod_identity" {
     value = false
   }
 
+  set {
+    name  = "forceNameSpaced"
+    value = true
+  }
 }
