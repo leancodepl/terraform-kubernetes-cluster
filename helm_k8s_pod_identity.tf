6c9ec82b3ccf94220edccd62ef8245cda8f65d4f
Jakub Fijałkowski <jakub.fijalkowski@leancode.pl>
2020-09-03 13:35:27 +0200

    Revert the namespace changes

--- helm_k8s_pod_identity.tf
+++ helm_k8s_pod_identity.tf
@@ -1,37 +1,39 @@
-resource "kubernetes_namespace" "aad_pod_identity" {
-  metadata {
-    name = "aad-pod-identity"
-  }
-}
+# TODO: make this namespaced when they release new version of the chart
+#resource "kubernetes_namespace" "aad_pod_identity" {
+#metadata {
+#name = "aad-pod-identity"
+#}
+#}
 
 resource "helm_release" "aad_pod_identity" {
   count = var.deploy_aad_pod_identity ? 1 : 0
 
   name       = "aad-pod-identity"
-  namespace  = kubernetes.kubernetes_namespace.aad_pod_identity.metadata[0].name
   repository = "https://raw.githubusercontent.com/Azure/aad-pod-identity/master/charts"
   chart      = "aad-pod-identity"
   version    = "2.0.1"
+
+  #namespace  = kubernetes.kubernetes_namespace.aad_pod_identity.metadata[0].name
 
   set {
     name  = "mic.tag"
     value = "1.6.2"
   }
 
-  set {
-    name  = "mic.leaderElection.namespace"
-    value = kubernetes.kubernetes_namespace.aad_pod_identity.metadata[0].name
-  }
+  #set {
+  #name  = "mic.leaderElection.namespace"
+  #value = kubernetes.kubernetes_namespace.aad_pod_identity.metadata[0].name
+  #}
 
   set {
     name  = "nmi.tag"
     value = "1.6.2"
   }
 
-  set {
-    name  = "nmi.micNamespace"
-    value = kubernetes.kubernetes_namespace.aad_pod_identity.metadata[0].name
-  }
+  #set {
+  #name  = "nmi.micNamespace"
+  #value = kubernetes.kubernetes_namespace.aad_pod_identity.metadata[0].name
+  #}
 
   set {
     name  = "installCRDs"
