a5887d0b9a85bbd5db5bfd62809360d73fd28537
Jakub Fijałkowski <jakub.fijalkowski@leancode.pl>
2020-11-26 14:18:29 +0100

    Rewrite how kuba-state-metrics is configured

--- apps_kubestatemetrics.tf
+++ apps_kubestatemetrics.tf
@@ -10,6 +10,15 @@ resource "kubernetes_manifest" "kube_state_metrics_ns" {
   }
 }
 
+locals {
+  kube_state_metrics_config = {
+    "resources.limits.cpu"      = "200m",
+    "resources.limits.memory"   = "300Mi",
+    "resources.requests.cpu"    = "100m",
+    "resources.requests.memory" = "200Mi",
+  }
+}
+
 resource "helm_release" "kube_state_metrics" {
   count = var.deploy_kube_state_metrics ? 1 : 0
 
@@ -20,20 +29,11 @@ resource "helm_release" "kube_state_metrics" {
 
   namespace = kubernetes_manifest.kube_state_metrics_ns[0].object.metadata.name
 
-  set {
-    name  = "resources.requests.cpu"
-    value = "100m"
-  }
-  set {
-    name  = "resources.requests.memory"
-    value = "200Mi"
-  }
-  set {
-    name  = "resources.limits.cpu"
-    value = "200m"
-  }
-  set {
-    name  = "resources.limits.memory"
-    value = "300Mi"
+  dynamic "set" {
+    for_each = local.kube_state_metrics_config
+    content {
+      name  = set.key
+      value = set.value
+    }
   }
 }
