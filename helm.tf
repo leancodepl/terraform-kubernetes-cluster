dcb49729bbf0712218a46f766ecee0b0f3ee8c3f
Jakub Fijałkowski <jakub.fijalkowski@leancode.pl>
2020-12-21 13:20:17 +0100

    Remove `load_config_file` in helm provider

--- helm.tf
+++ helm.tf
@@ -4,6 +4,5 @@ provider "helm" {
     client_certificate     = base64decode(azurerm_kubernetes_cluster.cluster.kube_admin_config.0.client_certificate)
     client_key             = base64decode(azurerm_kubernetes_cluster.cluster.kube_admin_config.0.client_key)
     cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.cluster.kube_admin_config.0.cluster_ca_certificate)
-    load_config_file       = false
   }
 }
