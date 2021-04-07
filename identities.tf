b643675b7403c5a56cf1aaa19b8dac50d658075e
Jakub Fijałkowski <jakub.fijalkowski@leancode.pl>
2021-04-07 17:10:27 +0200

    Add missing identities

--- identities.tf
+++ identities.tf
@@ -11,3 +11,14 @@ resource "azurerm_role_assignment" "service_contributor" {
   principal_id         = azurerm_user_assigned_identity.cluster_identity.principal_id
 }
 
+resource "azurerm_role_assignment" "aad_managed_identity_operator" {
+  scope                = azurerm_kubernetes_cluster.cluster.node_resource_group
+  role_definition_name = "Managed Identity Operator"
+  principal_id         = azurerm_kubernetes_cluster.cluster.kubelet_identity[0].object_id
+}
+
+resource "azurerm_role_assignment" "aad_vm_contributor" {
+  scope                = azurerm_kubernetes_cluster.cluster.node_resource_group
+  role_definition_name = "Virtual Machine Contributor"
+  principal_id         = azurerm_kubernetes_cluster.cluster.kubelet_identity[0].object_id
+}
