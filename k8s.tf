219676d0e61ef53ddaec7ebadc4d1c5f2242401e
Jakub Fijałkowski <jakub.fijalkowski@leancode.pl>
2021-04-07 12:31:43 +0200

    Switch to Managed Identity

--- k8s.tf
+++ k8s.tf
@@ -26,18 +26,17 @@ resource "azurerm_kubernetes_cluster" "cluster" {
     type = "VirtualMachineScaleSets"
   }
 
-  service_principal {
-    client_id     = azuread_service_principal.service.application_id
-    client_secret = random_password.service_secret.result
+  identity {
+    type                      = "UserAssigned"
+    user_assigned_identity_id = azuread_service_principal.service.application_id
   }
 
   role_based_access_control {
     enabled = true
 
     azure_active_directory {
-      client_app_id     = azuread_application.client.application_id
-      server_app_id     = azuread_application.server.application_id
-      server_app_secret = random_password.server_secret.result
+      managed                = true
+      admin_group_object_ids = [var.cluster_config.access.admin_access_group]
     }
   }
 
@@ -79,9 +78,6 @@ resource "azurerm_kubernetes_cluster" "cluster" {
   tags = local.tags
 
   depends_on = [
-    azuread_service_principal_password.server_secret,
-    azuread_service_principal_password.service_secret,
-    azuread_service_principal.client,
     azurerm_role_assignment.service_contributor
   ]
 }
