b102b3e47d8dff586d444afd4256dae881891b25
Jakub Fijałkowski <jakub.fijalkowski@leancode.pl>
2021-09-21 09:48:31 +0200

    Update Helm charts

--- apps_external_dns.tf
+++ apps_external_dns.tf
@@ -38,7 +38,7 @@ resource "helm_release" "external_dns" {
   name       = "external-dns"
   repository = "https://charts.bitnami.com/bitnami"
   chart      = "external-dns"
-  version    = "5.4.4"
+  version    = "5.4.7"
 
   namespace = kubernetes_namespace.external_dns[0].metadata[0].name
 
