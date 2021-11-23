fd593876493ed4654db1654638a25341b0aac8b6
Jakub Fijałkowski <kuba@codinginfinity.me>
2021-11-23 17:38:25 +0100

    Fix `external-dns` version

--- apps_external_dns.tf
+++ apps_external_dns.tf
@@ -38,7 +38,7 @@ resource "helm_release" "external_dns" {
   name       = "external-dns"
   repository = "https://charts.bitnami.com/bitnami"
   chart      = "external-dns"
-  version    = "4.8.0"
+  version    = "5.4.8"
 
   namespace = kubernetes_namespace.external_dns[0].metadata[0].name
 
