9192c1999672070316e57d1024e4fee7406ed23f
Jakub Fijałkowski <kuba@codinginfinity.me>
2022-09-06 17:08:13 +0200

    Change the output of `clsuter_domain` plugin

--- cluster_domain/outputs.tf
+++ cluster_domain/outputs.tf
@@ -1,4 +1,9 @@
-output "name_servers" {
-  description = "A list of name servers for the zone."
-  value       = azurerm_dns_zone.cluster_domain.name_servers
+output "domain" {
+  description = "A description of the Azure domain resource."
+  value = {
+    zone_id             = azurerm_dns_zone.cluster_domain.id
+    zone_name           = azurerm_dns_zone.cluster_domain.name
+    resource_group_name = azurerm_dns_zone.cluster_domain.resource_group_name
+    name_servers        = azurerm_dns_zone.cluster_domain.name_servers
+  }
 }
