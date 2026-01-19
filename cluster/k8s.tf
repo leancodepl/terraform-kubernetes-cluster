7d4099246e9a5a571222dd01d96a7ec8543f4e52
Tomasz Laskowski <tomasz.laskowski@leancode.pl>
2026-01-19 15:13:04 +0100

    Allow maintenance_window_auto_upgrade configuration

--- cluster/k8s.tf
+++ cluster/k8s.tf
@@ -61,6 +61,29 @@ resource "azurerm_kubernetes_cluster" "cluster" {
 
   image_cleaner_interval_hours = 48
   node_os_upgrade_channel      = var.node_os_upgrade_channel
+
+  dynamic "maintenance_window_auto_upgrade" {
+    for_each = var.maintenance_window_auto_upgrade[*]
+    content {
+      interval     = maintenance_window_auto_upgrade.value.interval
+      duration     = maintenance_window_auto_upgrade.value.duration
+      frequency    = maintenance_window_auto_upgrade.value.frequency
+      day_of_week  = maintenance_window_auto_upgrade.value.day_of_week
+      day_of_month = maintenance_window_auto_upgrade.value.day_of_month
+      start_date   = maintenance_window_auto_upgrade.value.start_date
+      start_time   = maintenance_window_auto_upgrade.value.start_time
+      utc_offset   = maintenance_window_auto_upgrade.value.utc_offset
+      week_index   = maintenance_window_auto_upgrade.value.week_index
+      dynamic "not_allowed" {
+        for_each = maintenance_window_auto_upgrade.value.not_allowed[*]
+        content {
+          start = maintenance_window_auto_upgrade.value.not_allowed.start
+          end   = maintenance_window_auto_upgrade.value.not_allowed.end
+        }
+      }
+    }
+  }
+
   dynamic "maintenance_window_node_os" {
     for_each = var.maintenance_window_node_os[*]
     content {
